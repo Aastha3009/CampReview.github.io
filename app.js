143f6af5610b9a059e092cdc2743c9f155947474
Aastha3009 <96370690+Aastha3009@users.noreply.github.com>
2023-09-06 16:08:24 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,105 @@
+if (process.env.NODE_ENV !== "production") {
+	require("dotenv").config();
+}
+
+const express = require("express");
+const path = require("path");
+const mongoose = require("mongoose");
+const ejsMate = require("ejs-mate");
+const session = require("express-session");
+const flash = require("connect-flash");
+const ExpressError = require("./utils/ExpressError");
+const methodOverride = require("method-override");
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const User = require("./models/user");
+const mongoSanitize = require("express-mongo-sanitize");
+
+const userRoutes = require("./routes/user");
+const campgroundsRoutes = require("./routes/campground");
+const reviewsRoutes = require("./routes/reviews");
+const MongoStore = require("connect-mongo")(session);
+//const dbUrl="process.env.DB_Url";
+const dbUrl = "mongodb://127.0.0.1:27017/yelp-camp";
+
+mongoose.connect(dbUrl);
+const db = mongoose.connection;
+
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+	console.log("Database connected");
+});
+
+const app = express();
+
+app.engine("ejs", ejsMate);
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(mongoSanitize());
+
+const store = new MongoStore({
+	url: dbUrl,
+	secret: "thisshouldbeabettersecret!",
+	touchAfter: 24 * 60 * 60,
+});
+
+store.on("error", function (e) {
+	console.log("Store Error");
+});
+
+const sessionConfig = {
+	store,
+	name: "session",
+	secret: "thisshouldbeabettersecret!",
+	resave: false,
+	saveUninitialized: true,
+	cookie: {
+		httpOnly: true,
+		//secure:true,
+		expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+		maxAge: 1000 * 60 * 60 * 24 * 7,
+	},
+};
+
+app.use(session(sessionConfig));
+app.use(flash());
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.use((req, res, next) => {
+	res.locals.currentUser = req.user;
+	res.locals.success = req.flash("success");
+	res.locals.error = req.flash("error");
+	next();
+});
+
+app.use("/", userRoutes);
+app.use("/campgrounds", campgroundsRoutes);
+app.use("/campgrounds/:id/reviews", reviewsRoutes);
+
+app.get("/", (req, res) => {
+	res.render("home");
+});
+
+app.all("*", (req, res, next) => {
+	next(new ExpressError("Page Not Found", 404));
+});
+
+app.use((err, req, res, next) => {
+	const { statusCode = 500 } = err;
+	if (!err.message) err.message = "Oh No, Something Went Wrong!";
+	res.status(statusCode).render("error", { err });
+});
+
+app.listen(3000, () => {
+	console.log("Serving on port 3000");
+});
